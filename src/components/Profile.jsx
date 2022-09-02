54a261974d9bc068f48b323adae66be823535d23
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-02 10:40:54 +0530

    updated user profiles

---
+++ src/components/Profile.jsx
@@ -0,0 +1,37 @@
+import { Typography } from "@mui/material";
+import { Box, height } from "@mui/system";
+import React from "react";
+
+export default function Profile() {
+  return (
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          height: "500px",
+          boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+        }}
+      >
+        <Box
+          sx={{
+            width: "90%",
+            height: "100%",
+            margin: "auto",
+            border: "1px solid red",
+            marginTop: "15px",
+          }}
+        >
+            <Box sx={{width:"100%" , height:"150px",position:"relative"}}>
+                <img src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=" alt=""  width={"100%"} height={"100%"}/>
+                <Box sx={{width:"80px" , height:"80px" , backgroundColor:"orangered" , borderRadius:"8px" , position:"absolute" , top:"85%" , left:"5%"}}>
+                </Box>
+                <Box sx={{position:"absolute" , left:"40%"}}>
+                    <Typography>Kemito </Typography>
+                    <Typography sx={{fontSize:"15px" , color:"gray"}}>UI/UX Designer</Typography>
+                </Box>
+            </Box>
+        </Box>
+      </Box>
+    </>
+  );
+}
--- src/components/UserInfoAndActivity.jsx
+++ src/components/UserInfoAndActivity.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import Profile from './Profile'
 
 export default function UserInfoAndActivity() {
   return (
     <>
     
     <Box sx={{width:"25%" , heigth:"100%" , border:"1px solid blue"}}>
+      <Profile/>
             
      </Box>  
     </>
