9eeaaabf7a721b11b49cbc34ff93df5c4146c9aa
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-02 15:22:52 +0530

    added buttons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,7 @@
-import { TextField, Typography } from "@mui/material";
-import { Box, height } from "@mui/system";
+import { Button, Divider, TextField, Typography } from "@mui/material";
+import { Box, height, Stack } from "@mui/system";
+import PhotoIcon from "@mui/icons-material/Photo";
+import SmartDisplayIcon from "@mui/icons-material/SmartDisplay";
 import React from "react";
 
 export default function Profile() {
@@ -19,43 +21,92 @@ export default function Profile() {
             margin: "auto",
             border: "1px solid red",
             marginTop: "15px",
-            position: "relative"
+            position: "relative",
           }}
         >
-            <Box sx={{width:"100%" , height:"150px",position:"relative"}}>
-                <img src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=" alt=""  width={"100%"} height={"100%"}/>
-                <Box sx={{width:"80px" , height:"80px" , backgroundColor:"orangered" , borderRadius:"8px" , position:"absolute" , top:"85%" , left:"5%"}}>
-                </Box>
-                <Box sx={{position:"absolute" , left:"40%"}}>
-                    <Typography>Kemito </Typography>
-                    <Typography sx={{fontSize:"15px" , color:"gray"}}>UI/UX Designer</Typography>
-                </Box>
-
-                <Box sx={{width:"70%" , height:"70px" , border:"1px solid red", position:"absolute" , top:"150%" ,left:"10%" , display:"flex",justifyContent:"space-between"}}>
-                 <Box sx={{width:"30%"}}>
-                 <Typography sx={{fontSize:"20px", fontWeight:"bold"}}> 11K</Typography>
-                 <Typography sx={{fontSize:"14px", color:"gray"}}>Followeres</Typography>
-                 </Box>
-
-                 <Box sx={{width:"30%"}}>
-                 <Typography sx={{fontSize:"20px", fontWeight:"bold"}}> 1.4K</Typography>
-                 <Typography sx={{fontSize:"14px", color:"gray"}}>Following</Typography>
-
-                 </Box>
-                </Box>
-            </Box>
-            <input 
-            placeholder='What do you think ?'
-            style={{position:"absolute",top:"65%" , left:"5%" ,width:"80%", height: "70px",
-            outline: "none",
-            backgroundColor:"#fafafa",
-            border:"none",
-            padding:"10px",
-            borderRadius:"8px",
-        color:"black"}}
-            
-        
+          <Box sx={{ width: "100%", height: "150px", position: "relative" }}>
+            <img
+              src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q="
+              alt=""
+              width={"100%"}
+              height={"100%"}
             />
+            <Box
+              sx={{
+                width: "80px",
+                height: "80px",
+                backgroundColor: "orangered",
+                borderRadius: "8px",
+                position: "absolute",
+                top: "85%",
+                left: "5%",
+              }}
+            ></Box>
+            <Box sx={{ position: "absolute", left: "40%" }}>
+              <Typography>Kemito </Typography>
+              <Typography sx={{ fontSize: "15px", color: "gray" }}>
+                UI/UX Designer
+              </Typography>
+            </Box>
+
+            <Box
+              sx={{
+                width: "70%",
+                height: "70px",
+                border: "1px solid red",
+                position: "absolute",
+                top: "150%",
+                left: "10%",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box sx={{ width: "30%" }}>
+                <Typography sx={{ fontSize: "20px", fontWeight: "bold" }}>
+                  {" "}
+                  11K
+                </Typography>
+                <Typography sx={{ fontSize: "14px", color: "gray" }}>
+                  Followeres
+                </Typography>
+              </Box>
+
+              <Box sx={{ width: "30%" }}>
+                <Typography sx={{ fontSize: "20px", fontWeight: "bold" }}>
+                  {" "}
+                  1.4K
+                </Typography>
+                <Typography sx={{ fontSize: "14px", color: "gray" }}>
+                  Following
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+          <input
+            placeholder="What do you think ?"
+            style={{
+              position: "absolute",
+              top: "65%",
+              left: "5%",
+              width: "80%",
+              height: "70px",
+              outline: "none",
+              backgroundColor: "#fafafa",
+              border: "none",
+              padding: "10px",
+              borderRadius: "8px",
+              color: "black",
+            }}
+          />
+          <Stack direction="row" spacing={2} sx={{position:"absolute",top:"82%" , left:"10%"}}>
+            <Button variant="filled" startIcon={<PhotoIcon sx={{color:"blue"}} />} sx={{backgroundColor:"rgb(250,250,250)" , color:"black"}}>
+              Image
+            </Button>
+            <Button variant="filled" startIcon={<SmartDisplayIcon sx={{color:"#e84c89"}} />} sx={{backgroundColor:"rgb(250,250,250)" , color:"black"}}>
+              Video
+            </Button>
+          </Stack>
+         <Button variant="filled" startIcon={<SmartDisplayIcon/>} sx={{position:"absolute"  , top:"92%" , left:'10%' , width:"80%"}}>Streaming</Button>
         </Box>
       </Box>
     </>
